7257b6abe2f7585050e0c4fd8511510901ce1bf8
orthagonal <orthagonal1@gmail.com>
2020-04-02 00:42:36 +0000

    move try/catches and fix Body

--- src/http/get-media-000image/getFromS3.js
+++ src/http/get-media-000image/getFromS3.js
@@ -2,10 +2,15 @@ const AWS = require('aws-sdk');
 const { config } = require('@firstandthird/arc-rapptor');
 
 module.exports = async(folder, name) => {
-  const aws = new AWS.S3();
-  const params = {
-    Bucket: config.s3.bucket,
-    Key: `${folder}/${name}`
-  };
-  return aws.getObject(params).promise();
+  try {
+    const aws = new AWS.S3();
+    const params = {
+      Bucket: config.s3.bucket,
+      Key: `${folder}/${name}`
+    };
+    const result = await aws.getObject(params).promise();
+    return result;
+  } catch (e) {
+    return false;
+  }
 };
--- src/http/get-media-000image/index.js
+++ src/http/get-media-000image/index.js
@@ -7,10 +7,10 @@ const mime = require('mime-types');
 // eslint-disable-next-line require-await
 exports.handler = response(async req => {
   const imageName = req.pathParameters.image;
-  // see if it exists in /optimized
-  try {
-    // if so just return that
-    const existingOptimizedImage = await getFromS3(config.folderOptimized, imageName);
+  // see if it exists in optimizedFolder
+  // if so just return that
+  const existingOptimizedImage = await getFromS3(config.folderOptimized, imageName);
+  if (existingOptimizedImage) {
     return {
       headers: {
         'Content-Type': mime.lookup(imageName)
@@ -18,29 +18,27 @@ exports.handler = response(async req => {
       isBase64Encoded: true,
       body: existingOptimizedImage.Body.toString('base64')
     };
-  } catch (e) {
-    log(['miss'], `${imageName} not found in ${config.folderOptimized}, will attempt to locate it in ${config.folderOriginals}`);
   }
-  // if not, grab it from /originals
-  try {
-    const originalImage = await getFromS3(config.folderOriginals, imageName);
-    // optimize it:
-    try {
-      const imageBuffer = await optimize({ quality: [config.quality, config.quality] }, originalImage.Body);
-      // put it in /optimized
-      await uploadToS3(`${config.folderOptimized}/${imageName}`, imageBuffer);
-      return {
-        headers: {
-          'Content-Type': mime.lookup(imageName)
-        },
-        isBase64Encoded: true,
-        body: imageBuffer.Body.toString('base64')
-      };
-    } catch (serverError) {
-      log(['optimize'], serverError);
-      return reply.text('Internal Error', { statusCode: 500 });
-    }
-  } catch (e) {
+  log(['miss'], `${imageName} not found in ${config.folderOptimized}, will attempt to locate it in ${config.folderOriginals}`);
+  // if not, grab it from originalFolder
+  const originalImage = await getFromS3(config.folderOriginals, imageName);
+  if (!originalImage) {
     return reply.text('Not Found', { statusCode: 404 });
+  }
+  // optimize it:
+  try {
+    const imageBuffer = await optimize({ quality: [config.quality, config.quality] }, originalImage.Body);
+    // put it in /optimized
+    await uploadToS3(`${config.folderOptimized}/${imageName}`, imageBuffer);
+    return {
+      headers: {
+        'Content-Type': mime.lookup(imageName)
+      },
+      isBase64Encoded: true,
+      body: imageBuffer.toString('base64')
+    };
+  } catch (serverError) {
+    log(['optimize'], serverError);
+    return reply.text('Internal Error', { statusCode: 500 });
   }
 });
